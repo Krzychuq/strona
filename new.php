16bae4a354374851d7b96e94cb56460a497e4c86
Krzychuq <krzysiu-krzys7@wp.pl>
2022-05-20 13:30:12 +0200

    final

--- new.php
+++ new.php
@@ -8,8 +8,8 @@
 <link rel="icon" href="favicon.ico">
 <link rel="stylesheet" href=
 "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-<link rel="stylesheet" type="text/css" href="/slick/slick.css">
-<link rel="stylesheet" type="text/css" href="/slick/slick-theme.css">
+<link rel="stylesheet" type="text/css" href="slick/slick.css">
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css">
 
 <title>Nowa strona</title>
 </head>
@@ -41,9 +41,10 @@
                 <div class="row2">
                         <img id="logo" src="logo.png">
                                 <div class="nav_mobile">
-                                        <i class="bi bi-heart"style="color:black; font-weight:bold; font-size:40px; padding:5px; margin-top:auto; margin-bottom:auto;"></i>
-                                        <i class="bi bi-cart3" style="color:black; font-weight:bold; font-size:40px; padding:5px; margin-top:auto; margin-bottom:auto;"></i>
-                                        <i class="bi bi-list" style="color:black; font-weight:bold; font-size:40px; padding:5px; margin-top:auto; margin-bottom:auto;"></i>
+                                        <i id="btn_mobile" class="bi bi-heart" title="Ulubione"></i>
+                                        <i id="btn_mobile" class="bi bi-cart3" title="Koszyk" ></i>
+                                                <i id="menu_list_mobile" class="bi bi-list" title="Produkty" ></i>
+                                                        
                                 </div>       
                         <div class="row2-1">
                                 <input class="searchbar" type="text" name="text" autocomplete="off" 
@@ -55,7 +56,9 @@
                                 <span id="zl" style="margin-left:5px;"><i class="bi bi-caret-right-fill"></i></span>
                         </div>
                 </div>
-        
+                
+
+
                 <div class="nav_bottom">
                         <a class="a1" href="#">Nowość</a>
                                 <span id="puste" style="margin-left:5px;"></span>
@@ -143,10 +146,59 @@
                         <span id="puste" style="margin-left:5px;"></span>
                                 
                 </div>
+                
        </div>
 </div>
 
+<div class="lista_mobile">   
+        <li><a class="menu_a" href="#">Nowość</a></li>    
+        <li class="list_m1">Odzież</li>
+                <div class="meni1">
+                        <li>Bluzy</li>
+                        <li>Buty</li>
+                        <li>Koszule</li>
+                        <li>Koszulki</li>
+                        <li>Spodnie</li>
+                </div>
+        <li class="list_m2">Akcesoria</li> 
+                <div class="meni2">
+                        <li>Czapki</li>
+                        <li>Okulary</li>
+                        <li>Paski</li>
+                        <li>Zawieszki</li>
+                </div>
+        <li class="list_m3">Buty</li>
+                <div class="meni3">
+                        <li>Vans</li>
+                        <li>Emerica</li>
+                        <li>Nike</li>
+                        <li>Es</li>
 
+                </div>
+        <li class="list_m4">Deskorolka</li>
+                <div class="meni4">
+                        <li>Blaty</li>
+                        <li>Traki</li>
+                        <li>Kółka</li>
+                        <li>Gripy</li>
+                        <li>Łożyska</li>
+                </div>
+
+        <li class="list_m5">Snowboard</li> 
+                <div class="meni5">
+                        <li>Wiązania</li>
+                        <li>Deski</li>
+                        <li>Buty</li>
+                </div>
+        <li class="list_m6">Wyprzedaż</li>
+                <div class="meni6">
+                        <li>20%</li>
+                        <li>40%</li>
+                        <li>70%</li>
+                </div>
+        <li><a class="menu_a" href="#">Marki</a></li>
+
+</div>
 <!-- MENU -->
 <section class="photos slider">
         <div class="slajdy">
@@ -163,13 +215,35 @@
 <script src="ui/jquery-ui.min.js"></script>
 <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
         <script>
-        $(document).on('ready', function() {
-                $(".photos").slick({
-                        dots: true,
-                        vertical: true,
-                        centerMode: true,
-                        slidesToShow: 4,
-                        slidesToScroll: 2
+        $(document).ready( function() {
+                $("#menu_list_mobile").on('click', function() {
+                        $(".lista_mobile").slideToggle("slow");
                 });
+                        $(".list_m1").on('click', function() {
+                                $(".meni1").toggle();
+                        });
+                                $(".list_m2").on('click', function() {
+                                        $(".meni2").toggle();
+                                });
+        
+                                        $(".list_m3").on('click', function() {
+                                                $(".meni3").toggle();
+                                        });
+                        
+                                        $(".list_m4").on('click', function() {
+                                                $(".meni4").toggle();
+                                        });
+        
+                                $(".list_m5").on('click', function() {
+                                        $(".meni5").toggle();
+                                });
+        
+                        $(".list_m6").on('click', function() {
+                                $(".meni6").toggle();
+                        });
+        
+        
         });
+
+
         </script>
