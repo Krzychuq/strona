6c40eef9e38481c75af02c323942b55d7f198d89
Krzychuq <krzysiu-krzys7@wp.pl>
2022-05-24 23:01:27 +0200

    s

--- new.php
+++ new.php
@@ -4,18 +4,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta charset="UTF-8"/>
 <meta name="robotscontent=noindex,nofollow"/>
-<script src="https://kit.fontawesome.com/98a8f2243f.js" crossorigin="anonymous"></script>
-<script src="jquery-3.6.0.js"></script>
-<script src="ui/jquery-ui.min.js"></script>
 <link rel="stylesheet" href="ui/jquery-ui.css">
 <link rel="icon" href="favicon.ico">
+<link rel="stylesheet" href=
+"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+<link rel="stylesheet" type="text/css" href="slick/slick.css">
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css">
+
 <title>Nowa strona</title>
 </head>
 
 <body>
 
 <div class="promocja-bar">
-        <span>Promocja 20% na produkty!!</span>
+        <span id="promo">Promocja 20% na produkty!!</span>
 </div>
 
 
@@ -25,121 +27,222 @@
        <div class="rows">
                 <div class="row1">
                         <div class="info">
-                        <span class="info_spany">(+48) 884 321 940</span>
-                        <span id="puste" style="margin-left:20px;"></span>  
-                        <span class="info_spany">email@gmail.com</span>
-                        <span id="puste" style="margin-left:20px;"></span>    
-                        <span class="info_spany">Zaloguj</span>
-                        <span id="puste" style="margin-left:20px;"></span>     
-                        <span class="info_spany">Ulubione(x)</span>
-                        <span id="puste" style="margin-left:20px;"></span> 
+                                <span class="info_spany">(+48) 884 321 940</span>
+                                        <span id="puste" style="margin-left:20px;"></span>  
+                                <span class="info_spany">email@gmail.com</span>
+                                        <span id="puste" style="margin-left:20px;"></span>    
+                                <span class="info_spany">Zaloguj</span>
+                                        <span id="puste" style="margin-left:20px;"></span>     
+                                <span class="info_spany">Ulubione(0)</span>
                         </div>
                 </div>
 
                 <div class="row2">
-                        <img src="logo.png" style="width:250px; height:auto; float:left; padding:10px 20px; margin-left:40px; margin-top:10px;">
+                        <img id="logo" src="logo.png">
+                                <div class="nav_mobile">
+                                        <i id="btn_mobile" class="bi bi-heart" title="Ulubione"></i>
+                                        <i id="btn_mobile" class="bi bi-cart3" title="Koszyk" ></i>
+                                                <i id="menu_list_mobile" class="bi bi-list" title="Produkty" ></i>
+                                                        
+                                </div>       
                         <div class="row2-1">
-                                <input class="searchbar" type="text" name="text" autocomplete="off" placeholder="CZEGO SZUKASZ?">
-                                        <span style="margin-left:50px;"><i class="fa-solid fa-cart-shopping"></i></span>
-                                                        <span style="margin-left:10px ;"></span>
+                                <input class="searchbar" type="text" name="text" autocomplete="off" 
+                                placeholder="CZEGO SZUKASZ?">
+                                        <span style="margin-left:20px;"><i style="font-size:18px;" class="bi bi-cart3"></i></span>
+                                                        <span style="margin-left:0px ;"></span>
                                                 <span style="font-size:15px;">&#10102;</span>
-                                        <span style="margin-left:50px;">0.00 PLN </span>
-                                <span style="margin-left:5px;"><i class="fa-solid fa-angle-right"></i></span>
+                                        <span id="zl" style="margin-left:20px;">0.00 PLN </span>
+                                <span id="zl" style="margin-left:5px;"><i class="bi bi-caret-right-fill"></i></span>
                         </div>
                 </div>
-        
+                
+
+
                 <div class="nav_bottom">
-                        <div class="list1">
-                        <span id="1">Nowość</span>
-                                <div class="lista1">
-                                        <span>1</span>
+                        <a class="a1" href="#">Nowość</a>
+                                <span id="puste" style="margin-left:5px;"></span>
+                                        <span id="puste" style="margin-left:25px;"></span>
+                        <div class="dropdown1">
+                                <div id="1" class="opcja-1">
+                                        <span>Odzież    <i class="bi bi-caret-down-fill"></i></span>
+                                </div>
+                                        <div class="droplista1">
+                                                <li>Bluzy</li>
+                                                <li>Buty</li>
+                                                <li>Koszule</li>
+                                                <li>Koszulki</li>
+                                                <li>Spodnie</li>
+                                        </div>
+                        </div>
+                                <span id="puste" style="margin-left:5px;"></span>
+                                <span id="puste" style="margin-left:25px;"></span>
+                        <div class="dropdown2">
+                                <div class="opcja-2">
+                                        <span>Akcesoria    <i class="bi bi-caret-down-fill"></i></span>
+                                </div>
+
+                                <div class="droplista2">
+                                        <li>Czapki</li>
+                                        <li>Okulary</li>
+                                        <li>Paski</li>
+                                        <li>Zawieszki</li>
                                 </div>
                         </div>
-
                                 <span id="puste" style="margin-left:5px;"></span>
                                         <span id="puste" style="margin-left:25px;"></span>
-                        <span id="2" class="dropdown1">Odzież    <i class="fa-solid fa-angle-down"></i></span>
+                        <div class="dropdown3">
+                                <div class="opcja-3">
+                                        <span>Buty    <i class="bi bi-caret-down-fill"></i></span>
+                                </div>
+                                <div class="droplista3">
+                                        <li>Vans</li>
+                                        <li>Emerica</li>
+                                        <li>Nike</li>
+                                        <li>Es</li>
+                                </div>
+                        </div>
                                 <span id="puste" style="margin-left:5px;"></span>
                                         <span id="puste" style="margin-left:25px;"></span>
-                        <span id="3" class="dropdown2">Akcesoria    <i class="fa-solid fa-angle-down"></i></span>
+                        <div class="dropdown4">
+                                <div class="opcja-4">
+                                        <span>Deskorolka    <i class="bi bi-caret-down-fill"></i></span>
+                                </div>
+                                <div class="droplista4">
+                                        <li>Blaty</li>
+                                        <li>Traki</li>
+                                        <li>Kółka</li>
+                                        <li>Gripy</li>
+                                        <li>Łożyska</li>
+                                </div>
+                        </div>
                                 <span id="puste" style="margin-left:5px;"></span>
                                         <span id="puste" style="margin-left:25px;"></span>
-                        <span id="4" class="dropdown3">Buty    <i class="fa-solid fa-angle-down"></i></span>
+                        <div class="dropdown5">
+                                <div class="opcja-5">
+                                        <span>Snowboard    <i class="bi bi-caret-down-fill"></i></span>
+                                </div>
+                                <div class="droplista5">
+                                        <li>Wiązania</li>
+                                        <li>Deski</li>
+                                        <li>Buty</li>
+                                </div>
+                        </div>
                                 <span id="puste" style="margin-left:5px;"></span>
                                         <span id="puste" style="margin-left:25px;"></span>
-                        <span id="5" class="dropdown4">Deskorolka    <i class="fa-solid fa-angle-down"></i></span>
-                                <span id="puste" style="margin-left:5px;"></span>
-                                        <span id="puste" style="margin-left:25px;"></span>
-                        <span id="6" class="dropdown5">Snowboard    <i class="fa-solid fa-angle-down"></i></span>
-                                <span id="puste" style="margin-left:5px;"></span>
-                                        <span id="puste" style="margin-left:25px;"></span>
-                        <span id="7" class="dropdown6">Wyprzedaż    <i class="fa-solid fa-angle-down"></i></span>
-                                <span id="puste" style="margin-left:5px;"></span>
-                                        <span id="puste" style="margin-left:25px;"></span>
-                        <span id="8">Marki</span>
-                                <span id="puste" style="margin-left:5px;"></span>
+                        <div class="dropdown6">
+                                <div class="opcja-6">
+                                        <span>Wyprzedaż    <i class="bi bi-caret-down-fill"></i></span>
+                                </div>
+                                <div class="droplista6">
+                                        <li>20%</li>
+                                        <li>40%</li>
+                                        <li>70%</li>
+                                </div>
+                        </div>
+                        <span id="puste" style="margin-left:5px;"></span>
+                        <span id="puste" style="margin-left:25px;"></span>
+                                <a class="a2" href="#">Marki</a>
+                        <span id="puste" style="margin-left:5px;"></span>
                                 
                 </div>
+                
        </div>
 </div>
 
-
-<div class="lista1">
-                <span>1</span>
-        </div>
-                <div class="lista2">
-                        <span>2</span>
+<div class="lista_mobile">   
+        <li><a class="menu_a" href="#">Nowość</a></li>    
+        <li class="list_m1">Odzież</li>
+                <div class="meni1">
+                        <li>Bluzy</li>
+                        <li>Buty</li>
+                        <li>Koszule</li>
+                        <li>Koszulki</li>
+                        <li>Spodnie</li>
                 </div>
-                        <div class="lista3">
-                                <span>3</span>
-                        </div>
-                                <div class="lista4">
-                                        <span>4</span>
-                                </div>
-                        <div class="lista5">
-                                <span>5</span>
-                        </div>
-                <div class="lista6">
-                        <span>6</span>
+        <li class="list_m2">Akcesoria</li> 
+                <div class="meni2">
+                        <li>Czapki</li>
+                        <li>Okulary</li>
+                        <li>Paski</li>
+                        <li>Zawieszki</li>
                 </div>
-        <div class="lista7">
-                <span>7</span>
-        </div>
+        <li class="list_m3">Buty</li>
+                <div class="meni3">
+                        <li>Vans</li>
+                        <li>Emerica</li>
+                        <li>Nike</li>
+                        <li>Es</li>
 
+                </div>
+        <li class="list_m4">Deskorolka</li>
+                <div class="meni4">
+                        <li>Blaty</li>
+                        <li>Traki</li>
+                        <li>Kółka</li>
+                        <li>Gripy</li>
+                        <li>Łożyska</li>
+                </div>
 
+        <li class="list_m5">Snowboard</li> 
+                <div class="meni5">
+                        <li>Wiązania</li>
+                        <li>Deski</li>
+                        <li>Buty</li>
+                </div>
+        <li class="list_m6">Wyprzedaż</li>
+                <div class="meni6">
+                        <li>20%</li>
+                        <li>40%</li>
+                        <li>70%</li>
+                </div>
+        <li><a class="menu_a" href="#">Marki</a></li>
 
-<!-- MENU -->
-
-<div class="slajdy">
-        <img src="photos/1.jpg" alt="photo" style="width:fit-content; height:auto;">
-        <img src="photos/2.jpg" alt="photo" style="width:fit-content; height:auto;">
-        <img src="photos/3.jpg" alt="photo" style="width:fit-content; height:auto;">
 </div>
-
+<!-- MENU -->
+<section class="photos slider">
+        <div class="slajdy">
+                <img src="photos/1.jpg" alt="photo" >
+                <img src="photos/2.jpg" alt="photo" >
+                <img src="photos/3.jpg" alt="photo" >
+        </div>
+</section>
 
 </body>
 </html>
 
-
-<script>
-        $(document).ready(function(){
-                $(".dropdown1").hover(function(){
-                        $(".lista1").fadeToggle("3000");
-                });  
-                        $(".dropdown2").hover(function(){
-                                $(".lista2").fadeToggle("3000");
-                        }); 
-                                $(".dropdown3").hover(function(){
-                                        $(".lista3").fadeToggle("3000");
-                                }); 
-                                $(".dropdown4").hover(function(){
-                                        $(".lista4").fadeToggle("3000");
-                                }); 
-                        $(".dropdown5").hover(function(){
-                                $(".lista5").fadeToggle("3000");
-                        }); 
-                $(".dropdown6").hover(function(){
-                        $(".lista6").fadeToggle("3000");
-                });                 
+<script src="jquery-3.6.0.js"></script>
+<script src="ui/jquery-ui.min.js"></script>
+<script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
+        <script>
+        $(document).ready( function() {
+                $("#menu_list_mobile").on('click', function() {
+                        $(".lista_mobile").slideToggle("slow");
+                });
+                        $(".list_m1").on('click', function() {
+                                $(".meni1").toggle();
+                        });
+                                $(".list_m2").on('click', function() {
+                                        $(".meni2").toggle();
+                                });
+        
+                                        $(".list_m3").on('click', function() {
+                                                $(".meni3").toggle();
+                                        });
+                        
+                                        $(".list_m4").on('click', function() {
+                                                $(".meni4").toggle();
+                                        });
+        
+                                $(".list_m5").on('click', function() {
+                                        $(".meni5").toggle();
+                                });
+        
+                        $(".list_m6").on('click', function() {
+                                $(".meni6").toggle();
+                        });
+        
+        
         });
-</script>
+
+
+        </script>
