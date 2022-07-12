f3eb429f37ee1ba42a26b0d23777a21ffa13f9b2
Krzychuq <104900546+Krzychuq@users.noreply.github.com>
2022-07-12 08:32:45 +0200

    s

---
+++ bluzy.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<link rel="stylesheet" href="bluzy.css?dat=<?php echo strtotime(date("H:i:s")) ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta charset="UTF-8"/>
+<meta name="robotscontent=noindex,nofollow"/>
+<link rel="stylesheet" href="ui/jquery-ui.css">
+<link rel="icon" href="favicon.ico">
+<link rel="stylesheet" href=
+"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+<link rel="stylesheet" type="text/css" href="slick/slick.css">
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css">
+</head>
+
+<body>
+    <div class="contener">
+        <div class="banner">
+            <p>Promocja na wszystkie bluzy!!!</p>
+
+        </div>
+            <div class="logo">
+                    <p style="font-size: 35px; font-family:monospace; float:left;">SKLEP.LQ</p>
+                    
+                    <span id="search"><i class="bi bi-search"></i></span>
+            </div>
+        <div class="menu">
+            <li>KOBIETA</li>
+            <li>MĘŻCZYZNA</li>
+            <li>DZIECKO</li>
+            <li>URODA</li>
+            <li>NOWOŚĆ</li>
+        </div>
+
+    </div>
+
+</body>
+
+
+</html>
