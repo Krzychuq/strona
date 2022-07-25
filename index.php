0bfc35684c929647513499fa9701109341e40f89
Krzychuq <104900546+Krzychuq@users.noreply.github.com>
2022-07-25 14:25:53 +0200

    ji

--- index.php
+++ index.php
@@ -42,11 +42,15 @@
                 <span>AKWARIUM</span>
                     <div class="droplista1">
                     <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
                 </div>
             </div>
             <div class="dropdown2">
                     <span>SPRZĘT</span>
                         <div class="droplista2">
+                        <li>test</li>
+                        <li>test</li>
                         <li>test</li>
                     </div>
                 </div>      
@@ -54,17 +58,23 @@
                     <span>RYBY</span>
                     <div class="droplista3">
                         <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
                     </div>
                 </div>  
                 <div class="dropdown4">
                     <span>DEKORACJE</span> 
                     <div class="droplista4">
                         <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
                     </div>
                 </div> 
             <div class="dropdown5">
                     <span>NOWOŚCI</span>
                 <div class="droplista5">
+                     <li>test</li>
+                     <li>test</li>
                      <li>test</li>
                 </div>
             </div>  
@@ -76,9 +86,9 @@
                 <figure>
                     <img src="photos/skalar1.jpg"> 
                         <img src="photos/skalar2.jpg">
-                        <img src="photos/skalar3.jpg">
-                    <img  id="photo" src="photos/skalar4.jpg">
-                </figure>
+                            <img src="photos/skalar3.jpg">
+                        <img  id="photo" src="photos/skalar4.jpg">
+                    </figure>
                 <button class="lewy"><i class="fa fa-arrow-circle-left"></i> </button>
                 <button class="prawy"><i class="fa fa-arrow-circle-right"></i> </button>
             </div>
@@ -116,49 +126,87 @@
                     </div>
                 </div>
                 <!-- Opis szczegółowy produktu -->
-            <div class="opis">
-                <p>Opis</p><br>
-                <p>Skalar</p><br>
-                <p>-posis</p>
-                <p>-posis</p>
-                <p>-posis</p>
-                <p>-posis</p>
+                <div class="opis">
+                    <ul class="opis-btn">
+                        <button id="opis1-btn">Opis</button>
+                        <button id="opis2-btn">Szczegóły</button>
+                    </ul>
+                    <div class="opis1">
+                        <p>
+                            Kirysek Panda w akwarium
+                            Kiryski są rybkami stadnymi. Najlepiej trzymać je w grupach od 6szt wzwyż. 
+                            Cztery sztuki to absolutne minimum i wtedy wystarczy im akwarium 40 litrowe. 
+                            Na podłoże najlepiej użyć piasku. Jeżeli jednak z jakiegoś powodu nie chcemy samego piasku to na części dna można zrobić drobny żwirek.
+                            Ważne by nie był to żwirek o ostrych krawędziach. 
+                            Roślinki są bardzo dobrym rozwiązaniem, aczkolwiek niekoniecznym. 
+                            Dobrze jest zapewnić rybom zacienione miejsca. 
+                            Można to osiągnąć na różne sposoby. 
+                            Mogą to być roślinki pływające, korzeń, grota lub skały. 
+                            Moim zdaniem bardzo fajnym rozwiązaniem będzie taki kawałek kory. 
+                            Pamiętaj by przynajmniej część tafli wody była wolna od roślin.
+                        </p>
+                    </div>
+                    <div class="opis2">
+                        <table class="tabelka">
+                            <tr>
+                                <td style="font-weight: bold; width: 35%;">Dorasta do</td>
+                                <td>20cm</td>
+                            </tr>
 
-
-                
-
+                            <tr>
+                                <td style="font-weight: bold;">Wielkość akwarium</td>
+                                <td>80 litrów</td>
+                            </tr>
+                            <tr>
+                                <td style="font-weight: bold;">pH wody</td>
+                                <td>6-7</td>
+                            </tr>
+                            <tr>
+                                <td style="font-weight: bold;">Temperatura wody</td>
+                                <td>23-27°C</td>
+                            </tr>
+                        </table>
+                    </div>
             </div>
         </div>
-        <div class="podobne">
-            Podobne produkty
-        </div>
+            <div class="podobne">
+                <span>Podobne produkty</span>
+            </div>
 
     </div>
 <div class="footer">
 
 </div>
 </body>
-
-
 </html>
 
 <script>
 	$(document).ready(function(){
 		var x = 0;
     // w prawo przesuniecie
-		$('.prawy').click(function(){
-    
+    $('.prawy').click(function(){
+
         x= (x<=200)?(x+100):0;
-      $('figure').css('left', -x+'%');
-		});
-
-
+        $('figure').css('left', -x+'%');
+    });
      // w lewo przesuniecie
      $('.lewy').click(function(){
     
-        x= (x>=100)?(x-100):400;
-      $('figure').css('left', -x+'%');
+        x= (x>=100)?(x-100):300;
+        $('figure').css('left', -x+'%');
     });
+        $('#opis1-btn').click(function(){
+                $('.opis1').css('display','block');
+                $('.opis2').css('display','none');
+                $('#opis1-btn').css('border-top', '3px solid red')
+                $('#opis2-btn').css('border-top', 'none')
+            });
+        $('#opis2-btn').click(function(){
+            $('.opis2').css('display','block');
+            $('.opis1').css('display','none');
+            $('#opis1-btn').css('border-top', 'none')
+            $('#opis2-btn').css('border-top', '3px solid red')
+        });
 	});
     // Licznik produktów
     var licznik = 1;
@@ -172,4 +220,7 @@
                 licznik += 1;}
             document.getElementById("licznik").innerHTML = licznik;
         };
+
+
+
 </script>
