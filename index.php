cbe3755d2b84ef31914cf738dee457efb1398f84
Krzychuq <krzysiu-krzys7@wp.pl>
2022-08-03 00:04:52 +0200

    nowe

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <link rel="stylesheet" type="text/css" href="slick/slick.css">
 <link rel="stylesheet" type="text/css" href="slick/slick-theme.css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css"
+         rel = "stylesheet">
+      <script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
+      <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 </head>
 
 <body>
@@ -25,14 +29,14 @@
                 <div class="logo">
                     <p>Sklepik</p>
                 </div>
-                    <div class="search">
-                        <input type="text" class="icon1" value placeholder="Czego szukasz?">
-                        <input type="text" class="icon2" value>
-                    </div>
-                    <div class="account">
-                        <h4 id="ikony0"><a id="konto1" href="konto.php"><i class="bi bi-person-circle"></i></a></h4>
-                        <a id="konto2" href="#konto">Konto</a>
-                    </div>
+                <div class="search">
+                    <input type="text" class="icon1" value placeholder="Czego szukasz?">
+                    <input type="text" class="icon2" value>
+                </div>
+                <div class="account">
+                    <h4 id="ikony0"><a id="konto1" href="konto.php"><i class="bi bi-person-circle"></i></a></h4>
+                    <a id="konto2" href="#konto">Konto</a>
+                </div>
                 <div class="cart">
                     <h4 id="ikony0"><a id="koszyk1" href="koszyk.php"><i class="bi bi-basket2-fill"></i></a></h4>   
                     <a id="koszyk2" href="#koszyk">Koszyk</a>
@@ -42,58 +46,62 @@
             <div class="dropdown1">
                 <span>AKWARIUM</span>
                     <div class="droplista1">
-                    <li>Szkła</li>
-                    <li>Pokrywy</li>
+                        <li>Szkła</li>
+                        <li>Pokrywy</li>
                 </div>
             </div>
             <div class="dropdown2">
                     <span>SPRZĘT</span>
                         <div class="droplista2">
-                        <li>Filtry</li>
-                        <li>Grzałki</li>
-                        <li>termometry</li>
+                            <li>Filtry</li>
+                            <li>Grzałki</li>
+                            <li>Termometry</li>
                     </div>
                 </div>      
                 <div class="dropdown3">
-                    <a href="#ryby"><span>RYBY</span></a>
+                    <a href="#ryby" style="color:black;"><span>RYBY</span></a>
                 </div>  
                 <div class="dropdown4">
                     <span>DEKORACJE</span> 
-                    <div class="droplista4">
-                        <li>Roślinki</li>
-                        <li>Ozdoby</li>
-                        <li>Tła</li>
+                    <div class="droplista4"> 
+                            <li>Roślinki</li>
+                            <li>Ozdoby</li>
+                            <li>Tła</li>
                     </div>
                 </div> 
             <div class="dropdown5">
                     <span>NOWOŚCI</span>
                 <div class="droplista5">
-                     <li>Dekoracje</li>
-                     <li>Sprzęt</li>
-                     <li>Ryby</li>
-                     <li>Akwarium</li>
+                        <li>Dekoracje</li>
+                        <li>Sprzęt</li>
+                        <li>Ryby</li>
+                        <li>Akwarium</li>
                 </div>
             </div>  
         </div>
 
 
         <div class="prodcon">
-            <div id="produkt">
-                <figure>
-                    <img src="photos/skalar1.jpg"> 
-                        <img src="photos/skalar2.jpg">
-                            <img src="photos/skalar3.jpg">
-                        <img  id="photo" src="photos/skalar4.jpg">
-                    </figure>
-                <button class="lewy"><i class="fa fa-arrow-circle-left"></i> </button>
-                <button class="prawy"><i class="fa fa-arrow-circle-right"></i> </button>
+            <div class="produkt">
+                <ul class="thumbnails">
+                    <li><a href="#slide1"><img src="photos/skalar1.jpg" /></a></li>
+                    <li><a href="#slide2"><img src="photos/skalar2.jpg" /></a></li>
+                    <li><a href="#slide3"><img src="photos/skalar3.jpg" /></a></li>
+                    <li><a href="#slide4"><img src="photos/skalar4.jpg" /></a></li>
+                </ul>
+                <ul class="slides">
+                    <li id="slide1"><img src="photos/skalar1.jpg" alt="" /></li>
+                    <li id="slide2"><img src="photos/skalar2.jpg" alt="" /></li>
+                    <li id="slide3"><img src="photos/skalar3.jpg" alt="" /></li>
+                    <li id="slide4"><img src="photos/skalar4.jpg" alt="" /></li>
+                </ul>
             </div>
                 <div class="info">
                     <div id="tytul">
                         <h2>Skalar</h2>
                     </div>
                         <div id="cena">
-                            <p>10,99zł</p>
+                            <p>11zł</p>
                         </div>
                             <div id="rodzaj">
                                 <p>Rodzaj</p>
@@ -162,29 +170,29 @@
                 <span>Podobne produkty</span>
                 <div class="podobne_zdjecia">
                     <div class="item" id="1">
-                        <a href="mieczyk.php"><img style="width: 200px; height: 200px;" src="photos/mieczyk.jpg">
+                        <a href="mieczyk.php"><img style="width: 200px; height: 200px;" src="photos/mieczyk.jpg" alt="mieczyk">
                             <p>Mieczyk</p>
-                        <p>9zł/szt</p></a>
+                        <p style="font-weight:bold;">9zł/szt</p></a>
                     </div>
                         <div class="item" id="2">
-                            <a href="glonojad.php"><img style="width: 200px; height: 200px;" src="photos/glonojad.jpg">
+                            <a href="glonojad.php"><img style="width: 200px; height: 200px;" src="photos/glonojad.jpg" alt="glonojad">
                                 <p>Glonojad</p>
-                            <p>7zł/szt</p></a>
+                            <p style="font-weight:bold;">7zł/szt</p></a>
                         </div>
                             <div class="item" id="3">
-                                <a href="krewetka.php"><img style="width: 200px; height: 200px;" src="photos/krewetka.jpg">
+                                <a href="krewetka.php"><img style="width: 200px; height: 200px;" src="photos/krewetka.jpg" alt="krewetka">
                                     <p>Krewetka blue</p>
-                                <p>20zł/szt</p></a>
+                                <p style="font-weight:bold;">20zł/szt</p></a>
                             </div>
                         <div class="item" id="4">
-                            <a href="tetra.php"><img style="width: 200px; height: 200px;" src="photos/tetra.jpg">
+                            <a href="tetra.php"><img style="width: 200px; height: 200px;" src="photos/tetra.jpg" alt="tetra">
                                 <p>Tetra czarna</p>
-                            <p>14zł/szt</p></a>
+                            <p style="font-weight:bold;">14zł/szt</p></a>
                         </div>
                     <div class="item" id="5">
-                        <a href="gupik.php"><img style="width: 200px; height: 200px;" src="photos/gupik.jpg">
+                        <a href="gupik.php"><img style="width: 200px; height: 200px;" src="photos/gupik.jpg" alt="gupik">
                             <p>Gupik</p>
-                        <p>3zł/szt</p></a>
+                        <p style="font-weight:bold;">3zł/szt</p></a>
                     </div>
                 </div>
             </div>
@@ -193,44 +201,39 @@
 <div class="footer">
     <div class="klasa">
         <span class="footer-title">Kontakt</span>
-        <p style=" margin-top: 10px;"><i style="font-size:1.4em;" class="bi bi-envelope-fill"></i> przykladowy2@email.com</p>
-        <p><i style="font-size:1.4em;" class="bi bi-telephone-fill"></i> +43 123 456 789</p>
+        <p class="tekst-footer"><i style="font-size:1.4em; margin-right:5px;" class="bi bi-envelope-fill"></i> przykladowy2@email.com</p>
+        <p><i style="font-size:1.4em; margin-right:5px;" class="bi bi-telephone-fill"></i> +43 123 456 789</p>
     </div>
         <div class="klasa">
-            <span class="footer-title">Polityka prywatności</span>
+            <span class="footer-title">Informacje</span>
+            <ul class="tekst-footer">
+                <li>Płatności</li>
+                <li>Współpraca</li>
+            </ul>
         </div>
         <div class="klasa">
             <span class="footer-title">Regulamin</span>
-            <li style=" margin-top: 10px;">Zwroty</li>
-            <li>Zakupy online</li>
+            <ul class="tekst-footer">
+                <li>Zwroty</li>
+                <li>Zakupy online</li>
+                <li>Polityka prywatności</li>
+            </ul>
         </div>
     <div class="klasa">
         <span class="footer-title">Śledz nas</span>
-        <p style=" margin-top: 10px;font-size:20px;">
+        <p class="tekst-footer" style="font-size:20px;">
             <a href="#"><i class="bi bi-facebook"></i></a>
             <a href="#"><i class="bi bi-youtube"></i></a>
             <a href="#"><i class="bi bi-instagram"></i></a>
         </p>
     </div>
+    
 </div>
 </body>
 </html>
 
 <script>
 	$(document).ready(function(){
-		var x = 0;
-    // w prawo przesuniecie
-    $('.prawy').click(function(){
-
-        x= (x<=200)?(x+100):0;
-        $('figure').css('left', -x+'%');
-    });
-     // w lewo przesuniecie
-     $('.lewy').click(function(){
-    
-        x= (x>=100)?(x-100):300;
-        $('figure').css('left', -x+'%');
-    });
         $('#opis1-btn').click(function(){
                 $('.opis1').css('display','block');
                 $('.opis2').css('display','none');
@@ -256,7 +259,4 @@
                 licznik += 1;}
             document.getElementById("licznik").innerHTML = licznik;
         };
-
-
-
 </script>
